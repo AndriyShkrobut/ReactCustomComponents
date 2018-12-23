6d3cc251de1c145890f6013a6131f4655aef46b1
AndriyShkrobut <andyshkrobut0107@gmail.com>
2018-12-23 19:20:41 +0200

    Add components with ToDo App

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './NavBar.css';
-import MenuList from './sub_components/MenuList'
+import MenuList from './sub_components/MenuList';
 
-function NavBar(props) {
-console.log(props.logoSRC)
-  return (
-    <header className="heading">
-        <div className="navbar">
-            <a href="index.html" className="logo"><img src={props.logoSRC} alt="ID-Consult"/></a>
-            <MenuList section1="home" section2="services" section3="about" section4="contact us" />
-        </div>
-    </header>
-    )
-}
-
+class NavBar extends React.Component {
+    constructor() {
+        super();
+        this.state = { };
+    }
+    render() {
+        return (
+            <header className="heading">
+                <div className="navbar">
+                    <a href="index.html" className="logo"><img src={this.props.logoSRC} alt="ID-Consult"/></a>
+                    <MenuList section1="home" section2="services" section3="about" section4="contact us" />
+                </div>
+            </header>
+        );
+    };
+};
 
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import NavBar from './components/navbar/NavBar';
+import ToDoList from './components/todolist/ToDoList'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<NavBar logoSRC={require('./components/navbar/logo.svg')}/>, document.getElementById('topBar'));
+ReactDOM.render(<ToDoList />, document.getElementById('toDoList'));
 serviceWorker.unregister();
